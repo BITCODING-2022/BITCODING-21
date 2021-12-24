40133a6ff6da3d4e96720ff776defe1a2e34e6ec
희호 <92441524+heeheehoho@users.noreply.github.com>
2021-12-24 21:01:58 +0900

    Add files via upload

--- components/Template.js
+++ components/Template.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Template.css";
 
-const Template = ({children,todoLength}) => {
-return(
+const Template = ({ children, todoLength }) => {
+  return (
     <div className="Template">
-        <div className="title">오늘의 할 일({todoLength})</div>
-        <div>{children}</div>
+      <div className="title">TODOLIST </div>
+    
+      <div>{children}</div>
     </div>
-    );
+  );
 };
+
 export default Template;
-
-
