3d0f9c770dde1f31b0aac9d85e15d311825ac8ab
jisun0415 <88261608+jisun0415@users.noreply.github.com>
2021-12-25 23:36:25 +0900

    Add files via upload

---
+++ Control.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Control extends Component {
+    render(){
+      console.log('Subject render');
+      return (
+        <ul>
+          <li className="CREATELI"><a href="/create" className="CREATE"onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('create');
+          }.bind(this)}>create</a></li>
+          <li className="DELETELI"><input className="DELETE" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('delete');
+          }.bind(this)} type="button" value="delete"></input></li>
+        </ul>
+      );
+    }
+  }
+
+export default Control; 
---
+++ Subject.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Subject extends Component{
+    render(){
+      console.log('Subject render');
+      return(
+        <article>
+        <h1 className="FONT"><a href="/" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangePage(); //이부분을 메인 페이지 연결로 바꾸면 될듯
+        }.bind(this)}>{this.props.title}</a></h1>
+        {this.props.sub}
+      </article>
+      );
+    }
+  }
+  
+export default Subject;
